6949bbf927b703602b5cf98a460abfb30a7857b3
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-08-01 14:28:06 -0500

    lego button start

---
+++ src/lib/components/LegoButton.svelte
@@ -0,0 +1,153 @@
+<script lang="ts">
+	import { Button } from 'flowbite-svelte';
+	const clicked = () => {
+		alert('You clicked btn1.');
+	};
+	export let label: string;
+	export let color: string;
+</script>
+
+<Button on:click={clicked} color="none">
+	<div class="flex flex-col">
+		<div id="brick-wrapper">
+			<div class="brick" id="main">
+				<p>{label}</p>
+				<div class="bits">
+					<div class="bit"></div>
+					<div class="bit"></div>
+					<div class="bit"></div>
+					<div class="bit"></div>
+				</div>
+			</div>
+		</div>
+		<div id="brick-wrap-2">
+			<div class="brick-2" id="green">
+				<div class="bits-2">
+					<div class="bit-2"></div>
+					<div class="bit-2"></div>
+					<div class="bit-2"></div>
+					<div class="bit-2"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</Button>
+
+<!-- <div id="brick-wrap"> -->
+<!-- <div class="brick-wrapper">
+	<div class="brick" id="red">
+		<div class="bits">
+			<div class="bit"></div>
+			<div class="bit"></div>
+			<div class="bit"></div>
+			<div class="bit"></div>
+		</div>
+	</div>
+</div> -->
+
+<!-- <div id="brick-wrap-2">
+	<div class="brick-2" id="green">
+		<div class="bits-2">
+			<div class="bit-2"></div>
+			<div class="bit-2"></div>
+			<div class="bit-2"></div>
+			<div class="bit-2"></div>
+		</div>
+	</div>
+</div> -->
+
+<style>
+	/* #brick-wrap { */
+	#brick-wrapper {
+		cursor: pointer !important;
+		z-index: 10;
+		transform: perspective(1050px) translate3d(0px, 0px, 150px) rotateX(-10deg) scale(0.9, 0.9);
+		box-shadow: 0 70px 40px -20px rgba(0, 0, 0, 0.2);
+		transition: 0.4s ease-in-out transform;
+		position: relative;
+		display: block;
+		width: 124px;
+		padding-bottom: 8px;
+		overflow: hidden;
+	}
+
+	#brick-wrapper:hover {
+		cursor: pointer !important;
+		transform: translate3d(0px, 5px, -150px);
+	}
+
+	.brick {
+		cursor: pointer !important;
+		/* width: auto; */
+		width: 124px;
+		height: 28px;
+		display: block;
+		border-radius: 2px;
+		top: 8px;
+		position: relative;
+		background: #000;
+		display: flex;
+		justify-content: center;
+		padding-top: 0.25rem !important;
+		padding-bottom: 0.25rem !important;
+	}
+
+	.bits {
+		position: absolute;
+		top: -8px;
+	}
+
+	.bit {
+		/* width: auto; */
+		width: 19px;
+		height: 8px;
+		display: block;
+		background: #000;
+		position: relative;
+		float: left;
+		margin: 0 6px;
+		border-radius: 2px 2px 0 0;
+	}
+
+	#brick-wrap-2 {
+		position: relative;
+		display: block;
+		/* width: auto; */
+		width: 124px;
+		padding-bottom: 8px;
+		overflow: hidden;
+	}
+
+	.brick-2 {
+		/* width: auto; */
+		width: 124px;
+		height: 10px;
+		display: block;
+		border-radius: 2px;
+		margin: 0 auto;
+		top: 5px;
+		position: relative;
+		background: #000;
+	}
+
+	.bits-2 {
+		position: absolute;
+		top: -4px;
+	}
+
+	.bit-2 {
+		width: 19px;
+		height: 4px;
+		display: block;
+		background: #000; /* Changed to green for id="green" */
+		position: relative;
+		float: left;
+		margin: 0 6px;
+		border-radius: 2px 2px 0 0;
+	}
+	p {
+		color: white;
+		font-size: 0.75rem;
+		font-weight: 700;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,79 @@
-<script>
-	import Header from './Header.svelte';
+<script lang="ts">
 	import '../app.css';
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		ImagePlaceholder,
+		Skeleton,
+		TextPlaceholder,
+		Input,
+		Button,
+		Dropdown,
+		DropdownItem,
+		DropdownDivider
+	} from 'flowbite-svelte';
+	import { SearchOutline, ChevronDownOutline } from 'flowbite-svelte-icons';
+	import LegoButton from '$lib/components/LegoButton.svelte';
 </script>
 
-<div class="app">
-	<Header />
+<!-- <slot></slot> -->
 
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+<div class="relative px-8">
+	<Navbar class="fixed start-0 top-0 z-20 w-full border-b px-2 py-2.5 sm:px-4">
+		<NavBrand href="/">
+			<!-- <img src="/images/noun-lego-brick.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
+			<img src="/images/noun-lego-brick.svg" class="me-3 h-6 sm:h-9" alt="Lego Logo" />
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+				>Flowbite</span
+			>
+		</NavBrand>
+		<div class="flex md:order-2">
+			<LegoButton label="Get started" />
+			<!-- <NavHamburger /> -->
+		</div>
+		<div class="flex md:order-2">
+			<Button
+				color="none"
+				data-collapse-toggle="mobile-menu-3"
+				aria-controls="mobile-menu-3"
+				aria-expanded="false"
+				class="me-1 rounded-lg p-2.5 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700 md:hidden"
+			>
+				<SearchOutline class="h-5 w-5" />
+			</Button>
+			<div class="relative hidden md:block">
+				<div class="pointer-events-none absolute inset-y-0 start-0 flex items-center ps-3">
+					<SearchOutline class="h-4 w-4" />
+				</div>
+				<Input id="search-navbar" class="ps-10" placeholder="Search..." />
+			</div>
+			<NavHamburger />
+		</div>
+		<NavUl>
+			<NavLi href="/" active={true}>Home</NavLi>
+			<NavLi href="/about">About</NavLi>
+			<NavLi href="/docs/components/navbar">Navbar</NavLi>
+			<NavLi href="/pricing">Pricing</NavLi>
+			<NavLi class="cursor-pointer">
+				Dropdown<ChevronDownOutline class="ms-2 inline h-6 w-6 text-primary-800 dark:text-white" />
+			</NavLi>
+			<Dropdown class="z-20 w-44">
+				<DropdownItem href="/">Dashboard</DropdownItem>
+				<DropdownItem href="/docs/components/navbar">Settings</DropdownItem>
+				<DropdownItem href="/">Earnings</DropdownItem>
+				<DropdownDivider />
+				<DropdownItem href="/">Sign out</DropdownItem>
+			</Dropdown>
+			<NavLi href="/contact">Contact</NavLi>
+		</NavUl>
+	</Navbar>
+	<!-- <div style="height:300px;" class="overflow-scroll pb-16"> -->
+	<div class="overflow-scroll pb-16">
+		<Skeleton class="mb-8 mt-16" />
+		<ImagePlaceholder class="my-8" />
+		<TextPlaceholder class="my-8" />
+	</div>
 </div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
-</style>
