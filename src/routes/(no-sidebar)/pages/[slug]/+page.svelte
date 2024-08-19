19e13575540362b433f1a805054a773a3c83bc1f
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-08-19 13:58:18 -0500

    ok

--- src/routes/(no-sidebar)/pages/[slug]/+page.svelte
+++ src/routes/(no-sidebar)/pages/[slug]/+page.svelte
@@ -2,7 +2,8 @@
 	import { DarkMode } from 'flowbite-svelte'; // noqa: E402
 	import type { PageData } from './$types';
 	export let data: PageData;
-	DarkMode.init();
 </script>
 
+<!-- <DarkMode> -->
 <svelte:component this={data.content} />
+<!-- </DarkMode> -->
