412f6cbc1be60ab953595106cf13e262de702adb
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-08-19 12:45:46 -0500

    ok

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 export default {
 	plugins: {
+		//Some plugins, like tailwindcss/nesting, need to run before Tailwind,
 		tailwindcss: {},
+		//But others, like autoprefixer, need to run after,
 		autoprefixer: {}
 	}
 };
