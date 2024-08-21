7b4fd9a2faf26d285193319298cf931c37f7a6f1
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-08-20 19:38:46 -0500

    otay pankie

--- functions/counter.ts
+++ functions/counter.ts
@@ -1,12 +1,13 @@
-import { DurableObjectNamespace, PagesFunction } from "@cloudflare/workers-types";
+import { DurableObjectNamespace, PagesFunction } from '@cloudflare/workers-types';
 interface Env {
-    COUNTER: DurableObjectNamespace;
+	// Cloudflare
+	COUNTER: DurableObjectNamespace;
 }
 export const onRequestPost: PagesFunction<Env> = async (context) => {
-    const id = context.env.COUNTER.newUniqueId();
-    // const counter = context.env.COUNTER.idFromName("A");
-    const stub = context.env.COUNTER.get(id);
-  
-    // Pass the request down to the durable object
-    return stub.fetch(context.request);
-  }
+	const id = context.env.COUNTER.newUniqueId();
+	// const counter = context.env.COUNTER.idFromName("A");
+	const stub = context.env.COUNTER.get(id);
+
+	// Pass the request down to the durable object
+	return stub.fetch(context.request);
+};
