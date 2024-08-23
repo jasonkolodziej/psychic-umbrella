6ba983989ed8d78f145c65327a79242465e143aa
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-08-23 12:38:42 -0500

    main changes

---
+++ src/lib/components/admin-dashboard/noSidebar/layout.ts
@@ -0,0 +1,4 @@
+export const headerNavBar: string =
+	'fixed top-0 z-40 mx-auto w-full flex-none border-b border-gray-200 bg-white dark:border-gray-600 dark:bg-gray-800';
+
+export const footerDivClass: string = 'mx-auto max-w-screen-2xl pt-[70px]';
--- src/lib/components/blog/Navbar.svelte
+++ src/lib/components/blog/Navbar.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import Notifications from '$lib/utils/dashboard/components/NotificationList.svelte';
 	import AppsMenu from '$lib/utils/widgets/AppsMenu.svelte';
 	import UserMenu from '$lib/utils/widgets/UserMenu.svelte';
@@ -16,10 +16,23 @@
 	import { ChevronDownOutline } from 'flowbite-svelte-icons';
 	// import '../../app.pcss';
 	import Users from '$lib/data/users.json';
+	import type { RouteType } from '$lib/utils/routeTypes';
+	// titles: ['Lego News', 'Lego Sets', 'Lego Reviews', 'Recommended Sets', 'About Me']
+	const routes: RouteType[] = [
+		{ name: 'Lego News', href: '/news' },
+		{ name: 'Lego Sets', href: '/sets' },
+		{ name: 'Lego Reviews', href: '/reviews' },
+		{ name: 'Recommended Sets', href: '/sets' },
+		{ name: 'About Me', href: '/about' }
+	];
 
+	export let companyOrgName = 'Beyond Bricks';
 	export let fluid = true;
 	export let drawerHidden = false;
-	export let list = false;
+	export let list: RouteType[] = [];
+
+	// TODO: add dynamic routing
+	$: list = routes;
 </script>
 
 <Navbar {fluid} class="text-black" color="default" let:NavContainer>
@@ -29,17 +42,29 @@
 			class="m-0 me-3 md:block lg:hidden"
 		/>
 		<NavBrand href="/" class={list ? 'w-40' : 'lg:w-60'}>
-			<img src="/images/noun-lego-brick.svg" class="me-2.5 h-6 sm:h-8" alt="Flowbite Logo" />
-			<span
-				class="ml-px self-center whitespace-nowrap text-xl font-semibold dark:text-white sm:text-2xl"
-			>
-				Flowbite
+			<img src="/images/noun-lego-brick.svg" class="me-2.5 h-6 sm:h-8" alt="Logos" />
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white sm:text-2xl">
+				{companyOrgName}
 			</span>
 		</NavBrand>
 		<div class="hidden lg:block lg:ps-3">
 			{#if list}
 				<NavUl class="ml-2" activeUrl="/" activeClass="text-primary-600 dark:text-primary-500">
-					<NavLi href="/">Home</NavLi>
+					{#each list as { name, href, dropdown }, i}
+						{#if dropdown}
+							<NavLi class="cursor-pointer">
+								Dropdown
+								<ChevronDownOutline class="ms-0 inline" />
+							</NavLi>
+							<Dropdown class="z-20 w-44">
+								<DropdownItem href="#top">Item 1</DropdownItem>
+								<DropdownItem href="#top">Item 2</DropdownItem>
+								<DropdownItem href="#top">Item 3</DropdownItem>
+							</Dropdown>
+						{/if}
+						<NavLi {href}>{name}</NavLi>
+					{/each}
+					<!-- <NavLi href="/">Home</NavLi>
 					<NavLi href="#top">Messages</NavLi>
 					<NavLi href="#top">Profile</NavLi>
 					<NavLi href="#top">Settings</NavLi>
@@ -51,7 +76,7 @@
 						<DropdownItem href="#top">Item 1</DropdownItem>
 						<DropdownItem href="#top">Item 2</DropdownItem>
 						<DropdownItem href="#top">Item 3</DropdownItem>
-					</Dropdown>
+					</Dropdown> -->
 				</NavUl>
 			{:else}
 				<form>
