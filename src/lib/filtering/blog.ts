cc37896c09f30dc7de908946d205c74d411d17f9
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-08-29 13:59:09 -0500

    zach

---
+++ src/lib/filtering/blog.ts
@@ -0,0 +1,57 @@
+export type Nullable<T> = T | null;
+
+export interface Picture {
+	Description: string;
+	URL?: string;
+	Data?: Buffer;
+}
+
+export interface Discussion {
+	// Author of the discussion
+	Author: string;
+	// Comments
+	Comment: Array<string> | string;
+	// Replies to the comment / discussion
+	Replies?: Array<Discussion>;
+}
+
+export interface PostData<T> {
+	// Author of the post
+	Author: string;
+	// Author Title
+	AuthorTitle?: string;
+	// Title of the post
+	Title: string;
+	// Summary of the post
+	Summary: string;
+	// Content of the post
+	Content?: Array<string> | string | Buffer;
+	// Related
+	RelatedContent?: T | Array<T>;
+	// Discussion of the post
+	Discussion?: Discussion;
+}
+
+export class Post<T> implements PostData<T> {
+	Title: string;
+	Author: string;
+	AuthorTitle?: string | undefined;
+	Summary: string;
+	Content?: string | Buffer | string[] | undefined;
+	RelatedContent?: T | T[] | undefined;
+	// TODO: add author and title and summary
+	// Initial timestamp of the post entry
+	CreatedAt: Date;
+	// Latest timestamp when data was altered on the post
+	UpdatedAt: Date;
+	// Data of the post
+	Discussion?: Discussion;
+
+	constructor(Author: string, Title?: string, Summary?: string) {
+		this.Author = Author;
+		this.Title = Title ?? '';
+		this.Summary = Summary ?? '';
+		this.CreatedAt = new Date();
+		this.UpdatedAt = new Date();
+	}
+}
--- src/lib/filtering/zach.ts
+++ src/lib/filtering/zach.ts
@@ -2,12 +2,35 @@
 // x>50 - 200$
 // x>200$
 
+import type { Picture } from './blog';
+
+export interface PricingFilters {
+	// Price range $ 50 and below
+	FiftyAndBelow: boolean;
+	// Price range ($ 50 to $ 200]
+	OverFiftyToTwoHundred: boolean;
+	// Price range ($ 200 and above)
+	OverTwoHundred: boolean;
+}
+
 // Overall reviews
 // Sections
 // 1> Overall Rating 1-5
 // 1a> Build experience (difficulty during build)
 // 1b> Play experience (after build)
 // 1c> Value for money (tie out)
+
+export type RatingScore = 1 | 2 | 3 | 4 | 5;
+export interface Review {
+	// Based on the sub sections of the review
+	overallRating: RatingScore | null;
+	// Difficulty during build
+	BuildExperience: RatingScore | null;
+	// After build
+	PlayExperience: RatingScore | null;
+	// Value for money, used as a tie breaker for the overall rating
+	ValueForMoney: RatingScore | null;
+}
 
 // Lego Set
 // SKU id
@@ -20,5 +43,84 @@
 // ==> Description
 // Size
 // Rating
+
+export interface Price {
+	Value: number;
+	Currency: string;
+}
+
+export interface LegoKit {
+	// Unique identifier for the set
+	SKU: string | number;
+	// Name of the set
+	Name: string;
+	// Release date of the set
+	ReleaseDate?: Date;
+	// Price of the set
+	Price?: Price;
+	// Theme or category of the set
+	theme?: string;
+	// Function to get the category/theme of the set
+	Category(): string;
+	// Pictures of the set
+	Pictures?: Picture[];
+	// Number of pieces
+	Size?: number;
+	// Rating of the set
+	Rating?: Review;
+}
+
+export class LegoSet implements LegoKit {
+	SKU: string | number;
+	Name: string;
+	Pictures?: Picture[];
+	Size?: number;
+	Rating?: Review;
+
+	Category(): string {
+		return this.theme ?? 'Miscellaneous';
+	}
+
+	constructor(SKU: string | number, Name: string) {
+		this.SKU = SKU;
+		this.Name = Name;
+		// this.ReleaseDate = undefined;
+	}
+
+	set theme(theme: string) {
+		this.theme = theme;
+	}
+
+	get theme(): string | undefined {
+		return this.theme;
+	}
+
+	public AddPictures(pics: Picture | Picture[]) {
+		if (this.Pictures === undefined) {
+			this.Pictures = [];
+		}
+		if (typeof pics === 'object' && 'Description' in pics) {
+			this.Pictures.push(pics);
+		} else {
+			this.Pictures = pics;
+		}
+	}
+
+	set Price(price: Price) {
+		this.Price = price;
+	}
+
+	set ReleaseDate(d: Date) {
+		this.ReleaseDate = d;
+	}
+
+	get ReleaseDate(): Date | undefined {
+		return this.ReleaseDate;
+	}
+
+	public AddReview(r: Review) {
+		this.Rating = r;
+	}
+}
 
 // Search bar to cover all of the above
