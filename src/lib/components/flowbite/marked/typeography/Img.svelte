106e14472f62484698ec7656765997d7ea0f6041
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-09-01 10:19:43 -0500

    start

---
+++ src/lib/components/flowbite/marked/typeography/Img.svelte
@@ -0,0 +1,17 @@
+<!-- Img.svelte -->
+<script lang="ts">
+	import type { Tokens } from 'marked';
+	import type { MarkdownOptions, Renderers } from '@magidoc/plugin-svelte-marked';
+
+	export let token: Tokens.Image;
+	export const options: MarkdownOptions = undefined;
+	export const renderers: Renderers = undefined;
+</script>
+
+<img src={token.href} title={token.title} alt={token.text} class="custom-image" />
+
+<style>
+	.custom-image {
+		max-width: 50%;
+	}
+</style>
