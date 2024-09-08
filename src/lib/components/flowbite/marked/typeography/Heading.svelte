f2e8827979805819221f735c857cc30de33ad89e
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-09-08 17:14:16 -0500

    good

--- src/lib/components/flowbite/marked/typeography/Heading.svelte
+++ src/lib/components/flowbite/marked/typeography/Heading.svelte
@@ -8,25 +8,44 @@
 		type MarkdownOptions,
 		type Renderers
 	} from '@magidoc/plugin-svelte-marked';
-	import { Heading } from 'flowbite-svelte';
+	import { Heading, A as Link } from 'flowbite-svelte';
 	import type { ComponentProps } from 'svelte';
 	import type { HeadingLevel } from '$components/blog/ui/card';
+	import { twJoin } from 'tailwind-merge';
 
 	export let token: Tokens.Heading & ComponentProps<Heading> & { level: number };
 
 	// export let token: Tokens.Heading
 	export let options: MarkdownOptions;
 	export const renderers: Renderers = undefined;
+	let headingClass: string = 'text-gray-900 dark:text-white';
 	let tag: HeadingLevel = ('h' + token.depth) as HeadingLevel;
 	let id: string | undefined;
+	let href: string | undefined;
 	$: id = urlUtils.generatePathSegment(token.text, options.slugger);
+	$: headingClass = id ? twJoin('group', 'relative', headingClass) : headingClass;
+	$: href = '#' + id;
+	$: console.log('Heading id', id, 'class', headingClass);
 </script>
 
-<Heading bind:tag {id}>
+<!-- <span class="absolute -top-[140px]" {id}> -->
+<Heading bind:tag class={headingClass}>
 	<!-- {token.text} -->
 	<slot />
 	<!-- <MarkdownTokens tokens={token.tokens} {renderers} {options} /> -->
+	{#key id}
+		<span class="absolute -top-[140px]" {id}></span>
+		<Link
+			class="text-primary-700 dark:text-primary-700 ms-2 opacity-0 transition-opacity group-hover:opacity-100"
+			{href}
+			aria-label="Link to this section: {token.text}"
+		>
+			#
+		</Link>
+	{/key}
 </Heading>
+
+<!-- </span> -->
 
 <!-- <svelte:element this={`h${token.depth}`} {id}>
   <slot />
