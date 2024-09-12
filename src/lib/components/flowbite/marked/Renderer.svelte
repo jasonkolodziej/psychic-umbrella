8a481a21be400990b5aa92cd1be21c4374dbe5aa
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-09-12 11:12:56 -0500

    Revert "merge"
    This reverts commit ff3360c866015174ac85b06a86ce66a76c560e71.

--- src/lib/components/flowbite/marked/Renderer.svelte
+++ src/lib/components/flowbite/marked/Renderer.svelte
@@ -1,44 +1,20 @@
 <script lang="ts">
-	import Markdown, { type Renderers } from '@magidoc/plugin-svelte-marked';
-	import { marked } from 'marked';
-	// ? Step 1, import our custom extension
-	// import BackgroundColor from '$components/flowbite/marked/extensions/BackgroundColor.svelte';
-	import {
-		customOptions,
-		mappedRenderers as renderers
-	} from '$components/flowbite/marked/configuration';
-	// import Img from '$components/flowbite/marked/typeography/Img.svelte';
+	import Markdown from '@magidoc/plugin-svelte-marked';
+	import Img from '$components/flowbite/marked/typeography/Img.svelte';
 
-	//? Step 2, register our custom extension
-	marked.use(customOptions);
 	let source = `
-# One
-## Two
-### Three
-#### Four
-##### Five
-###### Six
-
-> This is a blockquote
-
-> **Bold blockquote**
-
-[This is a link](https://google.com)
-
-[**this is a bold link**](https://google.com)
-
-Here are the steps to set up svelte-marked plugin
+  # Setup
+  Here are the steps to set up svelte-marked plugin
   1. Install it
   2. Enjoy
       - If you enjoyed, make sure to star the repo!
-
 Amazing image:
 ![Look at this image](https://lh3.googleusercontent.com/2hDpuTi-0AMKvoZJGd-yKWvK4tKdQr_kLIpB_qSeMau2TNGCNidAosMEvrEXFO9G6tmlFlPQplpwiqirgrIPWnCKMvElaYgI-HiVvXc=w600)
+  `;
 
-:::background-color color="lightgray"
-Look at this gray background with **internal markdown support**!!
-:::
-`;
+	const renderers = {
+		img: Img
+	};
 </script>
 
 <Markdown bind:source {renderers} />
--- src/lib/components/flowbite/marked/typeography/Img.svelte
+++ src/lib/components/flowbite/marked/typeography/Img.svelte
@@ -2,26 +2,16 @@
 <script lang="ts">
 	import type { Tokens } from 'marked';
 	import type { MarkdownOptions, Renderers } from '@magidoc/plugin-svelte-marked';
-	import { Img } from 'flowbite-svelte';
-	import type { ComponentProps } from 'svelte';
 
-	export let token: Tokens.Image &
-		Tokens.Generic &
-		ComponentProps<Img> & { radiusBorder: boolean; retinaReady: boolean };
+	export let token: Tokens.Image;
 	export const options: MarkdownOptions = undefined;
 	export const renderers: Renderers = undefined;
-	const radiusBorder = token.radiusBorder ?? true;
-	// let srcset
-	let size = token.radiusBorder ? 'max-w-lg' : token.retinaReady ? 'w-full max-w-xl' : '';
-	let imgClass = token.radiusBorder || token.retinaReady ? 'rounded-lg' : '';
-
-	let caption: string = undefined;
 </script>
 
-<Img src={token.href} title={token.title} alt={token.text} bind:caption bind:size bind:imgClass />
+<img src={token.href} title={token.title} alt={token.text} class="custom-image" />
 
-<!-- <style>
+<style>
 	.custom-image {
 		max-width: 50%;
 	}
-</style> -->
+</style>
