38919b8414a9abc75862023b68a61bff1e42430c
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-09-11 22:06:54 -0500

    article overviews fixed

--- src/lib/components/blog/ui/section/section-blog.svelte
+++ src/lib/components/blog/ui/section/section-blog.svelte
@@ -3,12 +3,20 @@
 	import type { HTMLAttributes } from 'svelte/elements';
 	import { cn } from '$lib/carta/utils';
 	import { BlogBodyWrapper, BlogHead, Section } from 'flowbite-svelte-blocks';
+	import type { ComponentProps, SvelteComponent } from 'svelte';
+	import Article from '../article/article.svelte';
 
 	type $$Props = HTMLAttributes<HTMLParagraphElement> & {
 		title: string;
 		description: string;
 		sectionClass?: string;
+		articles?: Array<ComponentProps<Article>>;
 	};
+
+	// type $$Slots = {
+	// 	// default: {};
+	// 	articles: Array<SvelteComponent<Article>>;
+	// };
 
 	let className: $$Props['class'] = undefined;
 	// export let name: $$Props['name'] = undefined;
@@ -17,6 +25,7 @@
 		name === 'blog' ? 'pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900' : '';
 	export let title: $$Props['title'] = '';
 	export let description: $$Props['description'] = '';
+	export let articles: $$Props['articles'] = [];
 
 	export { className as class };
 	// export let href: string;
@@ -26,12 +35,22 @@
 	<BlogHead>
 		<svelte:fragment slot="h2">{title}</svelte:fragment>
 		<svelte:fragment slot="paragraph">
-			<p class="font-light text-gray-500 dark:text-gray-400 sm:text-xl">
+			<p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
 				{description}
 			</p>
 		</svelte:fragment>
 	</BlogHead>
 	<BlogBodyWrapper>
-		<slot />
+		{#if articles}
+			{#each articles as article}
+				<Article {...article} />
+				<!-- <svelte:component this={Article} {...article} /> -->
+			{/each}
+		{/if}
+		<!-- {#if $$slots.articles}
+			{#each $$slots.articles as s}
+				{s}
+			{/each}
+		{/if} -->
 	</BlogBodyWrapper>
 </Section>
