6f7273ef585463c370dd3038d1b14cc425b64ecd
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-09-17 12:44:07 -0500

    worker types generated

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,6 +2,29 @@
 import { redirect, type Handle } from '@sveltejs/kit';
 // import { handle as authenticationHandle } from './auth';
 import { sequence } from '@sveltejs/kit/hooks';
+// ? For cloudflare durable objects -- https://github.com/dario-piotrowicz/sveltekit-durable-object-local-usage-example/blob/5cf91ad3cbb8e5c3383ba1e73bd4176a3e554c54/sveltekit-app/src/hooks.server.ts
+import { dev } from '$app/environment';
+import { getPlatformProxy } from 'wrangler';
+/*
+  When developing, this hook will add proxy objects to the `platform` object which
+  will emulate any bindings defined in `wrangler.toml`.
+*/
+let platform: App.Platform;
+if (dev) {
+	platform = await getPlatformProxy();
+}
+
+export const plateformHandle = async ({ event, resolve }) => {
+	if (platform) {
+		event.platform = {
+			...event.platform,
+			...platform
+		};
+	}
+
+	return resolve(event);
+};
+// ? For cloudflare durable objects
 
 const oldHandle = async ({ event, resolve }) => {
 	// event.locals.user = await getCurrentUser(event.cookies.get('sessionid'));
@@ -32,4 +55,4 @@ const oldHandle = async ({ event, resolve }) => {
 // First handle authentication, then authorization
 // Each function acts as a middleware, receiving the request handle
 // And returning a handle which gets passed to the next function
-export const handle: Handle = oldHandle; // sequence(authenticationHandle, authorizationHandle)
+export const handle: Handle = sequence(plateformHandle, oldHandle);
