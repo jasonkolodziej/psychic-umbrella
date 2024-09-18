91d42a2ab83dc284363241d4faa1dd9ca7542e01
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-09-18 17:20:44 -0500

    ught

--- src/lib/filtering/blog.ts
+++ src/lib/filtering/blog.ts
@@ -1,6 +1,6 @@
-import type { XOR } from '$lib/utils/templates';
+import type { Without, XOR } from '$lib/utils/templates';
 import type { SvelteComponent } from 'svelte';
-// * use
+import { v7 as uuidv7 } from 'uuid'; // * use
 export interface Commenter {
 	name: Author['name'];
 	profilePicture: Author['profilePicture'];
@@ -8,7 +8,7 @@ export interface Commenter {
 
 // * use
 export interface Comment {
-	id: string;
+	id: string | typeof uuidv7;
 	commenter: Commenter;
 	date: string | Date | DateConstructor;
 	content: string;
@@ -29,73 +29,112 @@ export type Picture = {
 // 	// profilePicture: ProfilePicture | ProfilePicture['profilePicture'];
 // 	href?: string;
 // }
-export type Author = Picture & {
-	name: string;
-	title?: string;
-	href?: string;
-	get profilePicture(): Picture['profilePicture'] | Picture['imgSrc'];
-};
+//? use
+// export type Author = Picture & {
+// 	name: string;
+// 	title?: string;
+// 	href?: string;
+// 	get profilePicture(): Picture['profilePicture'] | Picture['imgSrc'];
+// };
+// ? maybe
+export type Author = Picture & { name: string; title?: string; href?: string };
 
 // * use
-export interface Blog {
+// export interface Blog {
+// 	// * blog id
+// 	id: string | typeof uuidv7;
+// 	// * blog title
+// 	title: string;
+// 	// * blog lead (header) or summary
+// 	lead?: string | undefined;
+// 	// * blog article or the summarized card of the blog
+// 	article: Article;
+// }
+
+//? Maybe
+export type BlogArticleIdentifier = {
 	// * blog id
-	id: string;
+	id: string | typeof uuidv7;
 	// * blog title
 	title: string;
 	// * blog lead (header) or summary
 	lead?: string | undefined;
-	// * blog article or the summarized card of the blog
-	article: Array<Article>;
-}
-export interface BlogPost extends Blog {
-	// * blog author
-	author: Author;
-	// * blog date
-	date: string | Date | DateConstructor;
-	isoDate: string | Date | DateConstructor;
-	// * blog content
-	content: string;
-	get asArticle(): Article;
-	get comments(): Array<Comment> | undefined;
-}
+};
+// export interface BlogPost extends Blog {
+// 	// * blog author
+// 	author: Author;
+// 	// * blog date
+// 	date: string | Date | DateConstructor;
+// 	isoDate: string | Date | DateConstructor;
+// 	// * blog content
+// 	content: string;
+// 	get asArticle(): Article;
+// 	get comments(): Array<Comment> | undefined;
+// }
+
+export type BlogPost = BlogArticleIdentifier &
+	Author & {
+		date: string | Date | DateConstructor;
+		isoDate: string | Date | DateConstructor;
+		content: string;
+		comments?: Array<Comment>;
+		article?: Article;
+	};
 
 // * use
-export interface Article {
+export type Article = {
 	// * article head (date aka.. when, icon, iconLabel)
 	head?: ArticleHead;
 	// * article body (title of blog, lead | summary of blog, titleHref | href to the blog)
 	body?: ArticleBody;
 	// * article author (title, `name` of the author, imgSrc | profilePicture, imgDescription | description of the author, moreInfoLabel, moreInfoHref)
 	author?: ArticleAuthor;
-}
-export interface ArticleHead {
+};
+export type ArticleHead = {
 	// * friendly how long ago the article was published
-	when: string;
+	when: Intl.RelativeTimeFormat | string;
 	// * icon of the article
 	icon?: typeof SvelteComponent;
 	// * icon label
 	iconLabel?: string;
-}
-export interface ArticleAuthor {
-	title: Author['name'];
-	imgSrc: Author['profilePicture'];
-	imgDescription?: string | Author['imgDescription'] | Picture['imgDescription'];
+};
+// Use
+// export interface ArticleAuthor {
+// 	title: Author['name'];
+// 	imgSrc: Author['profilePicture'];
+// 	imgDescription?: string | Author['imgDescription'] | Picture['imgDescription'];
+// 	moreInfoLabel?: string;
+// 	moreInfoHref?: string;
+// }
+
+export type ArticleAuthor = Author & {
 	moreInfoLabel?: string;
 	moreInfoHref?: string;
-}
+};
+
 export const ToArticleAuthor = (author: Author): ArticleAuthor => {
 	return {
+		name: author.name,
 		title: author.name,
-		imgSrc: author.profilePicture,
-		imgDescription: author.imgDescription ?? author.name + author.title
+		imgSrc: author.profilePicture ?? author.imgSrc,
+		imgDescription: author.imgDescription ?? author.name // + author.title
 	} satisfies ArticleAuthor;
 };
-export interface ArticleBody {
-	title: BlogPost['title'];
-	lead: BlogPost['lead'];
+export type ArticleBody = Without<{ id: string }, BlogArticleIdentifier> & {
 	titleHref?: string;
 	// (blog: BlogPost, titleHref?: string): ArticleBody;
-}
+};
+export const ToArticleHead = (blog: BlogPost): ArticleHead => {
+	// const iso = new Date(blog.isoDate as string).getTime();
+	// const now = Date.
+	return {
+		when: new Intl.RelativeTimeFormat('en', { numeric: 'auto' }).format(
+			((Date.parse(blog.isoDate as string) - Date.now()) / 1000) * 86400,
+			'days'
+		)
+	} as ArticleHead;
+};
+
 export const ToArticleBody = (blog: BlogPost, titleHref?: string): ArticleBody => {
 	return { title: blog.title, lead: blog.lead, titleHref: titleHref ?? undefined } as ArticleBody;
 };
--- src/routes/(blog)/+page.server.ts
+++ src/routes/(blog)/+page.server.ts
@@ -1,10 +1,26 @@
 import type { R2Bucket } from '@cloudflare/workers-types';
 import type { PageServerLoad } from './$types';
+import type { ArticleHead, Blogs } from '$lib/filtering/blog';
+import { json } from '@sveltejs/kit';
 // import { onRequestGet } from '$functions/blogs';
 // import { mdReader } from '$lib/mdReader';
 export const load: PageServerLoad = async ({ isDataRequest, request, platform }) => {
 	const context = platform?.ctx;
 	const blogsBucket: R2Bucket = await platform?.env.BLOGS_BUCKET.get('blogs'); //.get('handler');
+	if (!blogsBucket) {
+		// const blogsProto = new Blogs();
+		const articleHead = {
+			when: new Date().toISOString()
+		} as ArticleHead;
+		const newBucket = await platform?.env.BLOGS_BUCKET.put('blogs', JSON.stringify(articleHead), {
+			metadata: { 'Content-Type': 'application/json' }
+		});
+		const blogs = await platform?.env.BLOGS_BUCKET.get('blogs');
+		const blogsBody = await blogs?.body;
+		console.log(JSON.parse(blogsBody ?? ''));
+		console.log('newBucket', blogs, newBucket, blogsBody);
+		// throw new Error('Blogs bucket not found');
+	}
 	// const blogsStub = blogsDo.get(blogsId);
 	// const doResp = await blogsStub.fetch('http://0.0.0.0');
 	// const awaitedResponse = await blogsStub.fetch(request);
