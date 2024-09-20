340470cfa20246059a935e82114a8574730e7f8f
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-09-20 11:25:56 -0500

    otay

--- src/routes/(blog)/sets/[setId]/+page.server.ts
+++ src/routes/(blog)/sets/[setId]/+page.server.ts
@@ -1,5 +1,27 @@
 import type { PageServerLoad } from './$types';
+import { client, legoColorsList } from '$lib/rebrickable/client';
+import { env } from '$env/dynamic/private';
+// import type { Config } from '@hey-api/client-fetch';
 
-export const load: PageServerLoad = async ({ isDataRequest, request }) => {
+export const load: PageServerLoad = async ({ isDataRequest, request, platform }) => {
+	const testKey = platform?.env.REBRICKABLE_API_KEY || env.REBRICKABLE_API_KEY;
+	// console.log('testKey', testKey);
+	// configure internal service client
+	client.setConfig({
+		// set default base url for requests
+		baseUrl: 'https://rebrickable.com',
+		// set default headers for requests
+		headers: {
+			Authorization: `key ${testKey}`
+		}
+	});
+	//Using interceptors
+	// rebrickableClient.interceptors.request.use((request, options) => {
+	// 	request.headers.set('Authorization', `key ${testKey}`);
+	// 	return request;
+	// });
+
+	const { data } = await legoColorsList();
+	console.log('data', data);
 	return { blog: { title: 'Blog', posts: [] } };
 };
