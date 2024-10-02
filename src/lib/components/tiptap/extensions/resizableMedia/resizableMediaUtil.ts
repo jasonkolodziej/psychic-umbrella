7e7f5e8dc0d0cc20f8e2c06cbceb13bdfe87caef
Jason Kolodziej <42076058+jasonkolodziej@users.noreply.github.com>
2024-10-02 18:35:51 -0500

    cleaner

--- src/lib/components/tiptap/extensions/resizableMedia/resizableMediaUtil.ts
+++ src/lib/components/tiptap/extensions/resizableMedia/resizableMediaUtil.ts
@@ -113,32 +113,3 @@ export const resizableMediaActions: ResizableMediaAction[] = [
 		delete: (deleteNode) => deleteNode()
 	}
 ];
-
-export type ResizableMediaStateContext = Record<string, boolean>;
-
-// a unique identifier
-// in order to not have to come up with a name we can use an empty object
-const KEY = {};
-
-// in our setter we can pass in anything we want. It doesn't need to have
-// the same type as the actual context
-export const setResizableMediaContext = (items: ResizableMediaAction[]) => {
-	setContext<ResizableMediaStateContext>(KEY, {
-		// contexts are not reactive, in order to keep data in sync
-		// we can use stores inside a context
-		currentSlide: writable(0),
-		items
-	});
-};
-
-// our getter simply wraps the `getContext` function and its type declaration
-export const getResizableMediaContext = () => {
-	const context = getContext<ResizableMediaStateContext>(KEY);
-	if (context !== undefined) return context;
-
-	// TypeScript thinks the function will always return a value of type `SliderContext`,
-	// but it's not true in the case we forgot to call `setSliderContext` first.
-	// In this case it would return `undefined`, which could lead to an unexpected behavior,
-	// so instead we'll throw an error which will inform you about the issue.
-	throw new Error('you forgot to set the Slider context before accessing it');
-};
